3effd16fc0788a32fffce8c7e1d368a7a5846761
haytham918 <yunxuant@umich.edu>
2023-06-19 21:27:44 -0400

    Add the Coin Image

--- src/pages/Currency.js
+++ src/pages/Currency.js
@@ -1,13 +1,16 @@
 import './Currency.css'
-
+import coin from "../coin.svg"
 const Currency = () => {
   return(
+    <>
     <div>
-      <h1 className='header'>Currency</h1>
+      <h1 className='header'>Currency Conversion</h1>
+      <img src={coin} alt='Coin' className='coin' />
       <div className='currency-container'>
 
       </div>
     </div>
+    </>
   )
 }
 
