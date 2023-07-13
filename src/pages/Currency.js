b1a052649948a55e195ad748489cd61ed9ee5d69
haytham918 <yunxuant@umich.edu>
2023-07-12 22:43:56 -0400

    Add to and exchange rate

--- src/pages/Currency.js
+++ src/pages/Currency.js
@@ -5,7 +5,10 @@ import { useState } from 'react'
 import Select from 'react-select'
 const Currency = () => {
   const [value_before, setEnterValue] = useState(null);
+  const [value_after, setAfterValue] = useState(null);
   const [before, setBefore] = useState(null);
+  const [after, setAfter] = useState(null);
+  const [exchangeRate, setRate] = useState(null);
   const inputHandler = (e) => {
     setEnterValue(e.target.value);
   };
@@ -49,6 +52,42 @@ const Currency = () => {
             Convert!
           </button>
           <img src={arrow} alt="Arrow" className="arrow" />
+        </div>
+
+        <div className="to-container">
+          <h4>To: </h4>
+          {value_after === null ? null : (
+            <div className="result-container">
+              <p className="result-text">{value_after}</p>
+            </div>
+          )}
+          <div className="kind-dropdown">
+            <Select
+              options={[]}
+              isDisabled={true}
+              value={after}
+              styles={{
+                control: (baseStyles) => ({
+                  ...baseStyles,
+                  borderColor: "green",
+                  borderRadius: "0.5rem",
+                  fontSize: "120%",
+                  textAlign: "center",
+                  fontWeight: "bold",
+                  fontFamily: "Verdana",
+                }),
+              }}
+            />
+          </div>
+        </div>
+
+        <div className="ratio-container">
+          <h4>Exchange Rate: </h4>
+          {after === null || value_after === null ? null : (
+            <div className="formula-container">
+              <p className="formula-text">{exchangeRate}</p>
+            </div>
+          )}
         </div>
       </div>
       </>
