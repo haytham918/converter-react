7faf2ba125503666bfa6fea4626c68ada25edd23
haytham918 <yunxuant@umich.edu>
2023-07-25 09:33:36 -0400

    Move useEffect to App

--- src/App.js
+++ src/App.js
@@ -1,27 +1,53 @@
-import Units from './pages/Units';
-import './App.css';
-import NavBar from './NavBar';
-import Currency from './pages/Currency';
-import Time from './pages/Time';
-import { Routes, Route } from 'react-router-dom';
+import Units from "./pages/Units";
+import "./App.css";
+import NavBar from "./NavBar";
+import Currency from "./pages/Currency";
+import Time from "./pages/Time";
+import { Routes, Route } from "react-router-dom";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
-const  App = () => {
+const App = () => {
+  const symbolsURL =
+    "http://data.fixer.io/api/symbols?access_key=0ccba43ed82b96bca5e8206f5f1f094a";
+  const latestURL =
+    "http://data.fixer.io/api/latest?access_key=0ccba43ed82b96bca5e8206f5f1f094a";
+  const [countryList, setCountryList] = useState({});
+  const [exchangeList, setExchangeList] = useState({});
+  useEffect(() => {
+    getData();
+  }, []);
+
+  async function getData() {
+    const result = await axios.get(symbolsURL);
+    const latestResult = await axios.get(latestURL);
+    if (latestResult.data.success) {
+      const data = result.data.symbols;
+      const latestData = latestResult.data.rates;
+      setCountryList(data);
+      setExchangeList(latestData);
+    }
+  }
   return (
-    <div className='overall'>
-      <div className='top-bar'>
-    <NavBar className='top-bar'/>
+    <div className="overall">
+      <div className="top-bar">
+        <NavBar className="top-bar" />
+      </div>
+      <div className="page-container">
+        <Routes>
+          <Route path="/" Component={Units} />
+          <Route path="/units" Component={Units} />
+          <Route
+            path="/currency"
+            render={() => (
+              <Currency countryList={countryList} exchangeList={exchangeList} />
+            )}
+          />
+          <Route path="/time" Component={Time} />
+        </Routes>
+      </div>
     </div>
-    <div className = 'page-container'>
-    <Routes>
-      <Route path='/' Component={Units}/>
-      <Route path='/units' Component={Units}/>
-      <Route path='/currency' Component={Currency}/>
-      <Route path='/time' Component={Time}/>
-    </Routes>
-    </div>
-    </div>
-   
   );
-}
+};
 
 export default App;
